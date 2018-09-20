ae82579b615728564f2fc7ddb2cc7f976df4efaa
Dionisia Montanez <dmontanez15@gmail.com>
2018-09-19 19:39:09 -0700

    new tasks can be created using a form.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,16 @@ class TasksController < ApplicationController
   end
 
   def new
-    
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(action: params[:task][:action],
+                    description: params[:task][:description])
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get '/tasks', to: 'tasks#index'
   # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  root 'tasks#index'
   resources :tasks, only: [:index, :show, :new, :create]
 end
