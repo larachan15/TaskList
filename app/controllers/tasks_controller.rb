78badb5018cfa1420fad63e4b3e0290ef5219ae3
Dionisia Montanez <dmontanez15@gmail.com>
2018-09-19 18:39:15 -0700

    updated index and show routes to use database.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,10 @@
-# TASKS = [
-#   { task: "Do something"},
-#   { task: "Do something else"},
-#   { task: "eat dessert"}
-# ]
-#
-# TASKS = ["clean room", "do laundry", "make lunch", "grocery shopping"]
-
 class TasksController < ApplicationController
   def index
-    return @tasks = TASKS.all
+    @tasks = Task.all
   end
 
   def show
     task_id = params[:id].to_i
-    @task = TASKS[task_id]
-    if @task.nil?
-      head :not_found
-    end
+    @task = Task.find(task_id)
   end
 end
--- db/migrate/20180918183228_create_tasks.rb
+++ db/migrate/20180918183228_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :task_action
+      t.string :action
       t.string :description
 
       t.timestamps
